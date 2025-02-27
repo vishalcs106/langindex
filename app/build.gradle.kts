29780b972b3482863b4739a1cd4ca0186c2313e5
vishalcs106 <vishalcs106@rocketmail.com>
2025-02-27 06:35:48 +0530

    Modified dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
 }
 
 dependencies {
-    implementation(project(":langindex"))
-    //implementation(libs.langindex)
+    //implementation(project(":langindex"))
+    implementation(libs.langindex)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
